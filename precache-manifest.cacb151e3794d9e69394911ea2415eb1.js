eb670e9f2d0e87c68f6ea17a3eff72cf7e550633
Oleg <salahovol@ya.ru>
2019-07-09 16:18:42 +0600

    Updates

---
+++ precache-manifest.cacb151e3794d9e69394911ea2415eb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5be06bb306d342912347",
+    "url": "/static/js/main.5be06bb3.chunk.js"
+  },
+  {
+    "revision": "8af1d5efd96522105252",
+    "url": "/static/js/2.8af1d5ef.chunk.js"
+  },
+  {
+    "revision": "5be06bb306d342912347",
+    "url": "/static/css/main.f74f1824.chunk.css"
+  },
+  {
+    "revision": "6724f44dccf56b067b79577608337265",
+    "url": "/index.html"
+  }
+];
